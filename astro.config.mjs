865c3323ae66789c7da133b673150b81625b197f
aleixtm13 <aleixtm13>
2025-09-05 18:46:39 +0100

    add: basic dashboard

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // ...otras configuraciones
+  server: {
+    host: true, // Esto hace que escuche en 0.0.0.0
+  }
+});
